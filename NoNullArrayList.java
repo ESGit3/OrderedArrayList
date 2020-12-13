10a415998de910812cadc71afca5525d05f27bd5
ESGit3 <ethanshan706@gmail.com>
2020-12-13 18:06:38 -0500

    nonullarraylist add

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,5 +1,16 @@
 import java.util.ArrayList;
 
 public class NoNullArrayList<T> extends ArrayList<T> {
+    public NoNullArrayList() {
+        super();
+    }
 
+    public boolean add(T value) {
+        if (value == null) {
+            throw new IllegalArgumentException("null is not a valid input");
+        }
+        return super.add(value);
+    }
 }
+
+
---
+++ Tester.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Tester {
+    public static void main(String[] args) {
+        NoNullArrayList<Integer> test = new NoNullArrayList<Integer>();
+        System.out.println(test.add(3));
+    }
+}
