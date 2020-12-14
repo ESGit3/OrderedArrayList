7afda06c2c0aeff0bc88f33d463f59a5fa0242db
ESGit3 <ethanshan706@gmail.com>
2020-12-13 20:35:28 -0500

    add 1

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,7 +8,17 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     }
 
     public boolean add(T value) {
-
+        if (value == null) {
+            throw new IllegalArgumentException("null is not a valid input");
+        }
+        for (int i = 0; i < this.size(); i++) {
+            if (value.compareTo(this.get(i)) <= 0) {
+               super.add(i, value);
+               return true;
+            }
+            super.add(0, value);
+            return true;
+        }
     }
 
     public void add(int i, T value) {
